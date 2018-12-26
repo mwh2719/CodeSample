627ec0722e1de2aca99c4636a83cec70dd72b489
mwh2719 <42761160+mwh2719@users.noreply.github.com>
2018-12-26 17:30:23 -0500

    Two school project code samples

---
+++ Main.cs
@@ -0,0 +1,366 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*this it the main class
+all the methods are called here and it is attached to the scene manager
+it checks for collisions
+it calls the move methods for all the objects
+it has timers so it knows when new objects can/are spawned
+it runs the game
+*/
+public class Main : MonoBehaviour {
+
+    //variables
+    private List<GameObject> asteriodList = new List<GameObject>();
+    public List<GameObject> bulletList = new List<GameObject>();
+    public List<GameObject> asteriodSpecList = new List<GameObject>();
+    float timerSpawn = 0.0f;
+    float timerShoot = 0.0f;
+    float timeToShoot = 1.0f;
+    float timeToSpawn = 1.5f;
+    float timerSpawnSpec = 0.0f;
+    float timeToSpawnSpec = 5.0f;
+    float countdown; 
+    private CollisionDetect collideClass;
+    public GameObject[] asteriodChoices = new GameObject[5];
+    private int rndm;
+    public GameObject ship;
+    public Vehicle shipClass;
+    private GameObject collidedAsteriod;
+    int score = 0;
+    public Camera cam;
+    float camWidth;
+    float camHeight;
+    bool immune;
+    float immuneTimer = 0.0f;
+    float timeToImmune = 2.0f;
+    public Texture2D shipSkin;
+    public GameObject specAsteroid;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        collideClass = new CollisionDetect();
+        camHeight = cam.orthographicSize * 2f;
+        camWidth = camHeight * cam.aspect;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Debug.Log(shipClass.lives);
+
+        //loop for just checking collsions between the ship and asteriods
+        foreach(GameObject a in asteriodList)
+        {
+            //checking to see if the ship collides with any asteriods
+            if (collideClass.CircleCollision(ship, a))
+            {
+                //taking a life away if it still has extra lives
+                if (shipClass.lives > 0 && !immune)
+                {
+                    //temporary saftey from getting hurt again
+                    immune = true;
+                    shipClass.lives--;
+                    //teleporting you to the origin if you get hit
+                    shipClass.ResetPos();
+                }
+                else if(shipClass.lives <= 0 && !immune)
+                {
+                    ship.SetActive(false);
+                }
+            } 
+        }
+
+        foreach (GameObject a in asteriodSpecList)
+        {
+            //checking to see if the ship collides with any special asteriods
+            if (collideClass.CircleCollision(ship, a))
+            {
+                //taking a life away if it still has extra lives
+                if (shipClass.lives > 0 && !immune)
+                {
+                    //temporary saftey from getting hurt again
+                    immune = true;
+                    shipClass.lives--;
+                    //teleporting you to the origin if you get hit
+                    shipClass.ResetPos();
+                }
+                else if (shipClass.lives <= 0 && !immune)
+                {
+                    ship.SetActive(false);
+                }
+            }
+        }
+
+        //checking to see if immune period is over
+        if (immune)
+        {
+            //used to show the ship is temp immune
+            ship.GetComponent<SpriteRenderer>().color = Color.green;
+            if(immuneTimer >= timeToImmune)
+            {
+                immune = false;
+                immuneTimer = 0.0f;
+            }
+        }
+        else
+        {
+            ship.GetComponent<SpriteRenderer>().color = Color.white;
+        }
+        if (ship.activeInHierarchy)
+        {
+
+            //loop for moving asteriods and other functions
+            foreach (GameObject s in asteriodList)
+            {
+                Asteriod temp = s.GetComponent<Asteriod>();
+                temp.Move();
+                //Debug.Log(temp.name);
+                //Debug.Log(temp.position);
+            }
+            foreach (GameObject b in bulletList)
+            {
+                Bullet temp = b.GetComponent<Bullet>();
+                temp.Move();
+            }
+            foreach (GameObject s in asteriodSpecList)
+            {
+                SpecialAsteroid temp = s.GetComponent<SpecialAsteroid>();
+                temp.Move(ship);
+            }
+        }
+
+        //checking collision between bullet and asteriods
+        for (int i = 0; i < bulletList.Count; i++)
+        {
+            for(int x = 0; x < asteriodList.Count; x++)
+            {
+                
+                if(collideClass.PointCollision( bulletList[i], asteriodList[x]))
+                {
+                    Destroy(bulletList[i]);
+                    bulletList.RemoveAt(i);
+                    //If it is the first time the asteriod is shot it runs this
+                    if (asteriodList[x].GetComponent<Asteriod>().asteriodLevel == 1)
+                    {
+                        GameObject[] temp = asteriodList[x].GetComponent<Asteriod>().BreakApart(asteriodList[x]);
+                        asteriodList.RemoveAt(x);
+                        foreach(GameObject g in temp)
+                        {
+                            asteriodList.Add(g);
+                        }
+                        score += 20;
+                    }
+                    //If it is the second time the asteriod is shot it runs this
+                    else if (asteriodList[x].GetComponent<Asteriod>().asteriodLevel == 2)
+                    {
+                        GameObject[] temp = asteriodList[x].GetComponent<Asteriod>().BreakApart(asteriodList[x]);
+                        asteriodList.RemoveAt(x);
+                        foreach (GameObject g in temp)
+                        {
+                            asteriodList.Add(g);
+                        }
+                        score += 50;
+                    }
+                    //If it is the third time the asteriod is shot, it destroys it
+                    else
+                    {
+                        Destroy(asteriodList[x]);
+                        asteriodList.RemoveAt(x);
+                        score += 90;
+                    }
+                }
+            }
+        }
+
+        //checking to see if the player shoot the special asteroid
+        for (int i = 0; i < bulletList.Count; i++)
+        {
+            for (int x = 0; x < asteriodSpecList.Count; x++)
+            {
+                if (collideClass.PointCollision(bulletList[i], asteriodSpecList[x]))
+                {
+                    Destroy(bulletList[i]);
+                    bulletList.RemoveAt(i);
+                    Destroy(asteriodSpecList[x]);
+                    asteriodSpecList.RemoveAt(x);
+                    score += 100;
+                }
+            }
+        }
+
+                    //getting rid of asteroids that are no longer on screen
+                    for (int x = 0; x < asteriodList.Count; x++)
+        {
+            if(asteriodList[x].transform.position.x > camWidth / 2 + 3 || asteriodList[x].transform.position.x < camWidth / -2 - 3 
+                || asteriodList[x].transform.position.y > camHeight / 2 + 3 || asteriodList[x].transform.position.y < camHeight /-2 - 3)
+            {
+                Destroy(asteriodList[x]);
+                asteriodList.RemoveAt(x);
+            }
+        }
+
+        //getting rid of bullets as they go off screen
+        for (int i = 0; i < bulletList.Count; i++)
+        {
+            if (bulletList[i].transform.position.x > camWidth / 2 + 1 || bulletList[i].transform.position.x < camWidth / -2 - 1
+                || bulletList[i].transform.position.y > camHeight / 2 + 1 || bulletList[i].transform.position.y < camHeight / -2 - 1)
+            {
+                Destroy(bulletList[i]);
+                bulletList.RemoveAt(i);
+            }
+        }
+        //Spawing asteriods as long as the player is still alive
+        if (ship.activeInHierarchy)
+        {
+            if (timerSpawn >= timeToSpawn)
+            {
+                Spawn();
+                timerSpawn = 0;
+            }
+            if (timerSpawnSpec >= timeToSpawnSpec)
+            {
+                SpawnSpecial();
+                timerSpawnSpec = 0;
+            }
+
+        
+            //methods for the vehicle class
+            shipClass.RotateVehicle();
+
+            shipClass.Drive();
+
+            shipClass.SetTransform();
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if (timerShoot >= timeToShoot)
+                {
+                    //Debug.Log("Calling Shoot");
+                    shipClass.Shoot();
+                    timerShoot = 0.0f;
+                }
+            }
+        }
+
+    }
+
+
+    //using this to spawn asteriods based on a timer and limit the speed of firing
+    private void FixedUpdate()
+    {
+        if (Time.time >= countdown)
+        {
+            //Debug.Log(Time.time + ">=" + countdown);
+            // Change the next update (current second+1)
+            countdown = Mathf.FloorToInt(Time.time) + 1;
+            timerSpawn++;
+            timerShoot++;
+            timerSpawnSpec++;
+            if (immune)
+            {
+                immuneTimer++;
+            }
+        }
+        
+    }
+
+
+    
+    
+
+    //making a method to spawn in asteriods with a random apperance
+    public void Spawn()
+    {
+        Vector3 pos = new Vector3(0,0,0);
+        float rndmSpot;
+        //Debug.Log("Spawning asteriod");
+        rndm = Random.Range(0, 5);
+        GameObject temp = Instantiate(asteriodChoices[rndm]);
+        Asteriod asteroidClass = temp.GetComponent<Asteriod>();
+        rndm = Random.Range(0, 4);
+        //Deciding which side of the screen the asteriod will come from
+        if(rndm == 0)
+        {
+            rndmSpot = Random.Range(camWidth / -2, camWidth / 2);
+            pos = new Vector3(rndmSpot, camHeight / 2 + 2,0);
+        }
+        else if(rndm == 1)
+        {
+            rndmSpot = Random.Range(camWidth / -2, camWidth / 2);
+            pos = new Vector3(rndmSpot, camHeight / -2 - 2,0);
+        }
+        else if(rndm == 2)
+        {
+            rndmSpot = Random.Range(camHeight / -2, camHeight / 2);
+            pos = new Vector3(camWidth / 2 + 2 , rndmSpot,0);
+        }
+        else if(rndm == 3)
+        {
+            rndmSpot = Random.Range(camHeight / -2, camHeight / 2);
+            pos = new Vector3(camWidth / -2 - 2, rndmSpot, 0);
+        }
+        asteroidClass.SetUp(pos);
+        asteriodList.Add(temp);
+    }
+
+    //Method for spawning the special seeking asteriod
+    public void SpawnSpecial()
+    {
+        Vector3 pos = new Vector3(0, 0, 0);
+        float rndmSpot;
+        //Debug.Log("Spawning asteriod");
+        GameObject temp = Instantiate(specAsteroid);
+        SpecialAsteroid asteroidClass = temp.GetComponent<SpecialAsteroid>();
+        rndm = Random.Range(0, 4);
+        //decding which side of the screen to spawn the asteriod on
+        if (rndm == 0)
+        {
+            rndmSpot = Random.Range(camWidth / -2, camWidth / 2);
+            pos = new Vector3(rndmSpot, camHeight / 2 + 2, 0);
+        }
+        else if (rndm == 1)
+        {
+            rndmSpot = Random.Range(camWidth / -2, camWidth / 2);
+            pos = new Vector3(rndmSpot, camHeight / -2 - 2, 0);
+        }
+        else if (rndm == 2)
+        {
+            rndmSpot = Random.Range(camHeight / -2, camHeight / 2);
+            pos = new Vector3(camWidth / 2 + 2, rndmSpot, 0);
+        }
+        else if (rndm == 3)
+        {
+            rndmSpot = Random.Range(camHeight / -2, camHeight / 2);
+            pos = new Vector3(camWidth / -2 - 2, rndmSpot, 0);
+        }
+        asteroidClass.SetUp(pos);
+        asteriodSpecList.Add(temp);
+    }
+
+
+    //making the gui
+    private void OnGUI()
+    {
+        int distanceBetween = 50;
+
+        GUI.skin.label.fontSize = 50;
+
+        GUI.Label(new Rect(90, 20, 200, 70), score.ToString());
+        //drawing lives
+        for (int i = 0; i < shipClass.lives; i++)
+        {
+            GUI.Label(new Rect(distanceBetween, 70, 50, 50), shipSkin);
+            distanceBetween += 50;
+        }
+
+        if (ship.activeInHierarchy == false)
+        {
+            GUI.skin.label.fontSize = 100;
+            GUI.Label(new Rect(250, 150, 1000, 500), "Game Over");
+        }
+
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,354 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ *Class to hold all the different movment methods
+ * Abstract, humans and zombies inherit from it
+ * Keeps GameObjects from falling off the terrian
+ * 
+ * */
+
+public abstract class Vehicle : MonoBehaviour {
+
+    //variables
+
+
+    // Vectors necessary for force-based movement
+    public Vector3 vehiclePosition;
+    public Vector3 acceleration;
+    public Vector3 direction;
+    public Vector3 velocity;
+
+
+    public Terrain ground;
+
+    private Vector3 center = new Vector3(25, 0, 25);
+
+    
+    //Variables for all the lists coming from the scene manager
+    public List<GameObject> humans;
+    public List<GameObject> zombies;
+    public List<GameObject> obstacles;
+    public List<GameObject> healing;
+    public Manager sceneMang;
+
+    //Used for obstacle avoidance
+    float radius = 4;
+    float safeDistance = 5;
+
+
+    // Floats
+    public float mass;
+    public float maxSpeed;
+
+
+    //Variables for debugging lines
+    protected GameObject future;
+    protected GameObject futurePos;
+    private bool clicked = false;
+    private bool debug = false;
+
+    float wanderAngle = 1.0f;
+
+
+    // Use this for initialization
+    protected void Start()
+    {
+
+        sceneMang = GameObject.Find("SceneManager").GetComponent<Manager>();
+        future = sceneMang.future;
+
+        //setting up list of obstacles for later use
+        obstacles = sceneMang.obstacles;
+
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+
+
+        //setting the bool to the opposite value when d is pressed
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+
+            if (clicked)
+            {
+                futurePos.SetActive(!debug);
+                debug = !debug;
+            }
+            else
+            {
+                futurePos = Instantiate(future, vehiclePosition, Quaternion.Euler(Vector3.zero));
+                clicked = true;
+                debug = !debug;
+            }
+        }
+
+        //Making game objects the represent the future position of the vehicle
+        if (debug)
+        {
+            futurePos.transform.position = this.transform.position + this.velocity;
+        }
+        
+
+
+        //keeping the human zombie lists updated
+        humans = sceneMang.humans;
+        zombies = sceneMang.zombies;
+        healing = sceneMang.healing;
+
+        
+
+        //checking the vehicles against all obstacles
+        for (int i = 0; i < obstacles.Count; i++)
+        {
+            ApplyForce(ClassObstacleAvoidance(obstacles[i]));
+        }
+
+        //Calling the movement methods
+        CalcSteeringForce();
+        Bounce();
+        velocity += acceleration * Time.deltaTime;
+        Vector3.ClampMagnitude(velocity, maxSpeed);
+        vehiclePosition += velocity * Time.deltaTime;
+        
+        //Moving the vehicle and getting variables reset for next update
+        direction = velocity.normalized;
+        acceleration = Vector3.zero;
+        transform.position = vehiclePosition;
+    }
+
+    //method used to make vehicles wander randomly
+    /*
+     * Wandering is accomplished by making an imaginary circle a little ways in front of the vehicle 
+     * and then choosing a point on that circle for it to seek and changing it slighlty each time the method is called
+     * */
+    protected Vector3 Wandering()
+    {
+        //setting up how far out the circle is
+        Vector3 circleCenter = velocity;
+        //if there is no velocity, just giving it a small push to get started
+        if(circleCenter == Vector3.zero)
+        {
+            circleCenter = new Vector3(1, 0, 1);
+        }
+        circleCenter.Normalize();
+        circleCenter *= 1;
+
+
+        //Setting up the circle size
+        Vector3 displacment = new Vector3(0, 0, -1);
+        displacment.Normalize();
+        displacment *= 2;
+
+        //Choosing a random point on the circle
+        displacment.x = Mathf.Cos(wanderAngle) * displacment.magnitude;
+        displacment.z = Mathf.Sin(wanderAngle) * displacment.magnitude;
+
+        wanderAngle += Random.Range(-.5f, .5f);
+
+        //returning the wander force to be used
+        Vector3 wanderForce = circleCenter + displacment;
+
+        wanderForce *= maxSpeed;
+
+        wanderForce -= velocity;
+
+        return wanderForce;
+    }
+
+    //abstract method used to make sure the characters stay seperated from each other
+    protected abstract Vector3 Separation();
+
+     
+    //Method to have vehicles detect obstacles in front of them and avoid them
+    protected  Vector3 ClassObstacleAvoidance(GameObject obst)
+    {
+        Vector3 vecCenter = obst.transform.position - this.transform.position;
+        float dotRight = Vector3.Dot(vecCenter, transform.right);
+        float dotForward = Vector3.Dot(vecCenter, transform.forward);
+        float radiSum = obst.GetComponent<Obstacle>().radius + radius;//variable to hold vehicle radius
+
+        //Checiking if the obstacle is behind it
+        if(dotForward < 0)
+        {
+            return Vector3.zero;
+        }
+
+        //Checking to see if the obstacle is too close
+        if (vecCenter.sqrMagnitude > (safeDistance * safeDistance)) //variable to hold distance when vehicle should start avoiding)
+        {
+            return Vector3.zero;
+        }
+
+        //Checking to see if the object is in the path of the vehicle
+        if(Mathf.Abs(dotRight) > radiSum)
+        {
+            return Vector3.zero;
+        }
+
+        //Checking to see if the obstcale is on the right or left then adding a force in the other direction
+        Vector3 desiredVelocity = Vector3.zero;
+        //Checking the left side
+        if(dotRight < 0)
+        {
+            desiredVelocity = transform.right * maxSpeed;
+            desiredVelocity *= 1 / vecCenter.magnitude;
+
+        }
+        //Checking the right side
+        else
+        {
+            desiredVelocity = -transform.right * maxSpeed;
+            desiredVelocity *= 1 / vecCenter.magnitude;
+        }
+
+        desiredVelocity *= 7;
+
+        return desiredVelocity - velocity;
+    }
+    
+    
+
+    // ApplyForce
+    // Receive an incoming force, divide by mass, and apply to the cumulative accel vector
+    public void ApplyForce(Vector3 force)
+    {
+        acceleration += force / mass;
+    }
+
+    // ApplyForce
+    // Receive an incoming force, divide by mass, and apply to the cumulative accel vector
+    public void ApplyGravityForce(Vector3 force)
+    {
+        acceleration += force;
+    }
+
+
+    // SEEK METHOD
+    // All Vehicles have the knowledge of how to seek
+    // They just may not be calling it all the time
+    /// <summary>
+    /// Seek
+    /// </summary>
+    /// <param name="targetPosition">Vector3 position of desired target</param>
+    /// <returns>Steering force calculated to seek the desired target</returns>
+    public Vector3 Seek(Vector3 targetPosition)
+    {
+        // Step 1: Find DV (desired velocity)
+        // TargetPos - CurrentPos
+        Vector3 desiredVelocity = targetPosition - vehiclePosition;
+
+        // Step 2: Scale vel to max speed
+        // desiredVelocity = Vector3.ClampMagnitude(desiredVelocity, maxSpeed);
+        desiredVelocity.Normalize();
+        desiredVelocity = desiredVelocity * maxSpeed;
+
+        // Step 3:  Calculate seeking steering force
+        Vector3 seekingForce = desiredVelocity - velocity;
+
+        // Step 4: Return force
+        return seekingForce;
+    }
+
+    /// <summary>
+    /// Overloaded Seek
+    /// </summary>
+    /// <param name="target">GameObject of the target</param>
+    /// <returns>Steering force calculated to seek the desired target</returns>
+    public Vector3 Seek(GameObject target)
+    {
+        return Seek(target.transform.position);
+    }
+
+    //making a method to pursue the vehicles future position rather than its current one
+    public Vector3 Pursue(GameObject target)
+    {
+        // Step 1: Find DV (desired velocity)
+        // TargetPos - CurrentPos
+        Vector3 desiredVelocity = ((target.transform.position + target.GetComponent<Vehicle>().velocity) - vehiclePosition) * 2;
+
+        // Step 2: Scale vel to max speed
+        // desiredVelocity = Vector3.ClampMagnitude(desiredVelocity, maxSpeed);
+        desiredVelocity.Normalize();
+        desiredVelocity = desiredVelocity * maxSpeed;
+
+        // Step 3:  Calculate seeking steering force
+        Vector3 seekingForce = desiredVelocity - velocity;
+
+        // Step 4: Return force
+        return seekingForce;
+    }
+
+    
+
+    //Creating a flee method
+    public Vector3 Flee(Vector3 targetPosition)
+    {
+
+        Vector3 desiredVelocity = vehiclePosition - targetPosition;
+
+
+        desiredVelocity.Normalize();
+        desiredVelocity = desiredVelocity * maxSpeed;
+
+
+        Vector3 fleeingForce = desiredVelocity - velocity;
+
+        return fleeingForce;
+    }
+
+    
+    //Method that uses a gameobject by plugging it in to the other flee method
+    public Vector3 Flee(GameObject target)
+    {
+        return Flee(target.transform.position);
+    }
+
+    //method to evade the vehicles future position
+    public Vector3 Evade(GameObject target)
+    {
+
+        Vector3 desiredVelocity = vehiclePosition - (target.transform.position + target.GetComponent<Vehicle>().velocity);
+
+
+        desiredVelocity.Normalize();
+        desiredVelocity = desiredVelocity * maxSpeed;
+
+
+        Vector3 fleeingForce = desiredVelocity - velocity;
+
+        return fleeingForce;
+    }
+
+
+    //method to make the steering force for the object
+    public abstract void CalcSteeringForce();
+
+    //Method to keep all the vehicles from falling off the terrian edges
+    //If they get to close to an edge they will simply seek the center again
+    public void Bounce()
+    {
+        if (vehiclePosition.x - 5 < 0)
+        {
+            ApplyForce(Seek(center) * 2);
+        }
+
+        if (vehiclePosition.x + 5 > 50)
+        {
+            ApplyForce(Seek(center) * 2);
+        }
+
+        if (vehiclePosition.z - 5 < 0)
+        {
+            ApplyForce(Seek(center) * 2);
+        }
+
+        if (vehiclePosition.z + 5 > 50)
+        {
+            ApplyForce(Seek(center) * 2);
+        }
+    }
+}
